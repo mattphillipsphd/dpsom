b33840b22d54fe9845cc6292f48fa2054ce49187
Matt Phillips <mattphillipsphd@gmail.com>
2020-03-20 01:05:38 -0400

    Modified hdf_convert.py to work on unzipped csv files, reformatted

--- eicu_preproc/hdf_convert.py
+++ eicu_preproc/hdf_convert.py
@@ -12,18 +12,22 @@ import glob as glob
 import ipdb
 import gc
 
-TABLES=["admissionDrug", "admissionDx", "allergy", "apacheApsVar", "apachePatientResult", "apachePredVar", "carePlanCareProvider", 
-        "carePlanEOL", "carePlanGeneral" , "carePlanGoal", "carePlanInfectiousDisease", "customLab", "diagnosis", "hospital", 
-        "infusionDrug", "intakeOutput", "lab", "medication", "microLab", "note", "nurseAssessment", "nurseCare", "nurseCharting", 
-        "pastHistory", "patient", "physicalExam", "respiratoryCare", "respiratoryCharting", "treatment", "vitalAperiodic",
-        "vitalPeriodic"]
+TABLES=["admissionDrug", "admissionDx", "allergy", "apacheApsVar",
+        "apachePatientResult", "apachePredVar", "carePlanCareProvider", 
+        "carePlanEOL", "carePlanGeneral" , "carePlanGoal",
+        "carePlanInfectiousDisease", "customLab", "diagnosis", "hospital", 
+        "infusionDrug", "intakeOutput", "lab", "medication", "microLab", "note",
+        "nurseAssessment", "nurseCare", "nurseCharting", 
+        "pastHistory", "patient", "physicalExam", "respiratoryCare",
+        "respiratoryCharting", "treatment", "vitalAperiodic", "vitalPeriodic"]
 
 def hdf_convert(configs):
     
     for table in TABLES:
 
         print("Processing table {}".format(table))
-        table_path=os.path.join(configs["source_data_dir"],"{}.csv.gz".format(table))
+        table_path=os.path.join(configs["source_data_dir"],
+                "{}.csv".format(table))
 
         if table=="lab":
             dtype_dict={"labresulttext": object}
@@ -44,24 +48,32 @@ def hdf_convert(configs):
         else:
             dtype_dict=None
 
-        df=pd.read_csv(table_path,quoting=csv.QUOTE_ALL,dtype=dtype_dict, compression="gzip")
-        df.to_hdf(os.path.join(configs["dest_dir"],"{}.h5".format(table)),key=configs["dset_key"],mode='w',format="table",
-                  data_columns=["patientunitstayid"],complevel=configs["hdf_comp_level"],complib=configs["hdf_comp_alg"])
+        df=pd.read_csv(table_path,quoting=csv.QUOTE_ALL,dtype=dtype_dict)
+        df.to_hdf(os.path.join(configs["dest_dir"],"{}.h5".format(table)),
+                key=configs["dset_key"],mode='w',format="table",
+                data_columns=["patientunitstayid"],complevel=configs[\
+                        "hdf_comp_level"],complib=configs["hdf_comp_alg"])
 
 if __name__=="__main__":
 
     parser=argparse.ArgumentParser()
 
     # Arguments
-    parser.add_argument("--dset_key", default="data", help="Data set key to write")
-    parser.add_argument("--hdf_comp_level", default=5, type=int, help="HDF compression level to use")
-    parser.add_argument("--hdf_comp_alg", default="blosc:lz4", help="HDF compression algorithm to use")
+    parser.add_argument("--dset_key", default="data",
+            help="Data set key to write")
+    parser.add_argument("--hdf_comp_level", default=5, type=int,
+            help="HDF compression level to use")
+    parser.add_argument("--hdf_comp_alg", default="blosc:lz4",
+            help="HDF compression algorithm to use")
 
     # Input paths
-    parser.add_argument("--source_data_dir", default="../data/csv", help="Source data directory with CSV tables") 
+    parser.add_argument("--source_data_dir", default="../data/csv",
+            help="Source data directory with CSV tables") 
 
     # Output paths
-    parser.add_argument("--dest_dir", default="../data/hdf", help="Destination directory where the HDF tables should be saved into")
+    parser.add_argument("--dest_dir", default="../data/hdf",
+            help="Destination directory where the HDF tables should be saved " \
+                    "into")
 
     args=parser.parse_args()
     configs=vars(args)
